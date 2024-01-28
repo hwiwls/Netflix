7a5bc6d7538cdc68a37bf160d7e6c4cdc68421a9
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-01-28 23:44:15 +0900

    Initial Commit

---
+++ Netflix/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by hwijinjeong on 1/28/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
